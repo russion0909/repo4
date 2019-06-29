8c0ba84c9f7c3d8fc0ceaea913cdb8693e07f635
russion <russion@yeah.net>
2019-06-29 20:06:09 +0800

    主干代码1

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Demo {
+
+    private Date date;
+
+    public static void main(String[] args) {
+        System.out.println("hello,git ");
+        System.out.println("hello,git ");
+        System.out.println("hello,git ");
+        System.out.println("hello,git ");
+
+
+
+    }
+}
